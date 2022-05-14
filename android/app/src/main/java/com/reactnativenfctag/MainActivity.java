9a752455623451afaf78a3e820e3ae8023f10e05
murakami <ek.h1.hy@gmail.com>
2022-05-14 12:06:33 +0900

    react navigation導入

--- android/app/src/main/java/com/reactnativenfctag/MainActivity.java
+++ android/app/src/main/java/com/reactnativenfctag/MainActivity.java
@@ -3,6 +3,7 @@ package com.reactnativenfctag;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ReactNativeNFCTag";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Button, Alert} from 'react-native';
-import NfcManager, {NfcEvents, NfcTech} from 'react-native-nfc-manager';
+import * as React from 'react';
+
+import AppNavigator from './AppNavigator';
 
 function App(props) {
-  const [hasNfc, setHasNfc] = useState(null);
-
-  useEffect(() => {
-    async function checkNfc() {
-      setHasNfc(await NfcManager.isSupported());
-    }
-
-    checkNfc();
-  }, []);
-
-  if (hasNfc === null) {
-    return null;
-  } else if (!hasNfc) {
-    return (
-      <View style={styles.wrapper}>
-        <Text>You device doesn't support NFC</Text>
-      </View>
-    );
-  }
-
-  const readTag = () => {
-    (async () => {
-      await NfcManager.requestTechnology(NfcTech.Ndef);
-      const tag = await NfcManager.getTag();
-      console.warn('Tag found', tag);
-    })()
-      .catch(error => {
-        console.warn('Oops!', error);
-      })
-      .finally(() => {
-        NfcManager.cancelTechnologyRequest();
-      });
-  };
-
-  const wirteTag = () => {
-    Alert.alert('', 'hello');
-  };
-
   return (
-    <View style={styles.wrapper}>
-      <Text>Hello NFC</Text>
-      <Button
-        onPress={readTag}
-        title="Read Tag"
-        color="blue"
-        accessibilityLabel="Learn more about this purple button"
-      />
-    </View>
+    <>
+      <AppNavigator />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
